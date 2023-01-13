91535064efdb85cada4dd058c25b45578f9f3415
Ella-Bakshi <anmolbakshi24es@gmail.com>
2023-01-13 19:46:32 +0530

    1.Learned to use memory stuff in bootsector 2. Learned to use global ofset "org"

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,37 +1,30 @@
-;13/01/2023 10:00am new boot sector
-;infinite loop for fun
-;loop: 
-;    jmp loop
+[org 0x7c00]
+mov ah,0x0e
 
-;$is current memory address (0) and 
-;$$ is the first memory address in given section(0 i think not sure)
-;times 510-($-$$) db 0
-;dw 0xAA55
+mov al,"1"
+int 0x10
+mov al,the_secret
+int 0x10
 
+mov al,"2"
+int 0x10
+mov al,[the_secret]
+int 0x10
 
-;13/01/2023 12:41pm new boot sector 
-mov ah,0x0e ; function to tell cpu to write stuff after this in tty mode
-mov al,"H" ;to put h in lower of extended pair a
-int 0x10 ;to make intrupt to video services of cpu
-mov al,"e"
-int 0x10 
-mov al,"l"
-int 0x10 
-int 0x10 
-mov al,"o"
-int 0x10 
-mov al," "
-int 0x10 
-mov al,"W"
+mov al,"3"
 int 0x10
-mov al,"o"
+mov bx,the_secret 
+add bx,0x7C00
+mov al,[bx]
 int 0x10
-mov al,"r"
+
+mov al,"4"
 int 0x10
-mov al,"l"
+mov al,[0x7c2b]
 int 0x10
-mov al,"d"
-int 0x10
+
+the_secret:
+    db "X"
 
 jmp $ ;infinite loop by jumping here 
 
